44e92601cd7c21bb87012c604ab834124b5252e3
Souksamlane Hugo <37270994+HSouksamlane@users.noreply.github.com>
2022-02-05 23:41:25 +0100

    push2

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace csharp
 {
@@ -12,77 +13,34 @@ namespace csharp
 
         public void UpdateQuality()
         {
+            var app = new Refactoring(Items);
+
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
+                var current_item = Items[i].Name;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                switch (current_item)
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+                    case "Aged Brie":
+                        app.Update_Brie_Quality(i);
+                        break;
 
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        app.Update_Concert_Ticket_Quality(i);
+                        break;
+
+                    case "Conjured Mana Cake":
+                        app.Update_Conjured_Item(i);
+                        break;
+
+                    default:
+                        app.Update_Common_Item(i);
+                        break;
                 }
+                
             }
         }
     }
---
+++ csharp/Refactoring.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Refactoring
+    {
+        IList<Item> Items;
+        public Refactoring(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void Update_Brie_Quality(int i)
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
+            if (Items[i].Quality < 50)
+            {
+                Items[i].Quality = Items[i].Quality + 1;
+                if (Items[i].SellIn < 0)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
+            }
+        }
+        public void Update_Concert_Ticket_Quality(int i)
+        {
+            if (Items[i].Quality < 50)
+            {
+                Items[i].Quality = Items[i].Quality + 1;
+
+                if (Items[i].SellIn < 11 && Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
+
+                if (Items[i].SellIn < 6 && Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
+            }
+            Items[i].SellIn = Items[i].SellIn - 1;
+
+            if (Items[i].SellIn < 0)
+            {
+                Items[i].Quality = Items[i].Quality - Items[i].Quality;
+            }
+        }
+        
+        public void Update_Common_Item(int i)
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
+            if (Items[i].Quality > 0)
+            {
+                Items[i].Quality = Items[i].Quality - 1;
+            }
+            if (Items[i].SellIn < 0 && Items[i].Quality > 0)
+            {
+                Items[i].Quality = Items[i].Quality - 1;
+            }
+        }
+
+        public void Update_Conjured_Item(int i)
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
+            if (Items[i].Quality > 0)
+            {
+                Items[i].Quality = Items[i].Quality - 2;              
+            }
+            if (Items[i].SellIn < 0 && Items[i].Quality > 0)
+            {
+                Items[i].Quality = Items[i].Quality - 2;
+            }
+        }
+    }
+}
